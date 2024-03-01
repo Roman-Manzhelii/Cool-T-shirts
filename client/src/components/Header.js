c53aee8228164fadf2cc7119fdc268749c1d2fc7
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2024-03-01 10:24:11 +0000

    footer components

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img src ="./photos/C.png" alt = "logo"/>
+        </header>
+    );
+};
+
+export default Header;
