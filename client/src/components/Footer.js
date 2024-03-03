bbaa9793a5cd8a127704a069c7b1335b7a0d2cf9
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2024-03-03 04:52:54 +0000

    updates

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,18 +7,18 @@ const Footer = () => {
         <footer>
             <div>
             <div className="sponsors">
-            <img src="../photos/aa.png" alt="logo"/>
-            <img src="../photos/bdf.jpg" alt="logo"/>
-            <img src="../photos/tally.jpg" alt="logo"/>
+            <img src="./photos/aa.png" alt="logo"/>
+            <img src="./photos/bdf.jpg" alt="logo"/>
+            <img src="./photos/tally.jpg" alt="logo"/>
             </div>
             <div className="additional-links">
                 <ul>
-                    <li><a href="/">H&M</a></li>
-                    <li><a href="/about">ZARA</a></li>
-                    <li><a href="/contact">Dolce & Gabana</a></li>
-                    <li><a href="/">SHIEN</a></li>
-                    <li><a href="/about">Burberry</a></li>
-                    <li><a href="/contact">FENDI</a></li>
+                    <li><a href="https://www2.hm.com/en_ie/index.html">H&M</a></li>
+                    <li><a href="https://www.zara.com/ie/">ZARA</a></li>
+                    <li><a href="https://www.dolcegabbana.com/en-ie/">Dolce & Gabana</a></li>
+                    <li><a href="https://eur.shein.com/">SHIEN</a></li>
+                    <li><a href="https://ie.burberry.com/">Burberry</a></li>
+                    <li><a href="https://www.fendi.com/ie-en/">FENDI</a></li>
                 </ul>
 
             </div>
